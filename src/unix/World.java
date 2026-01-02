e8804676a08ed86d93a8abeca3bc69afa5e95528
IsekaiRid <mainrpg62@gmail.com>
2026-01-02 20:06:27 +0700

    up

---
+++ src/unix/World.java
@@ -0,0 +1,9 @@
+package unix;
+
+import mindustry.mod.Mod;
+
+public class UnixGlobalWars extends Mod {
+    public UnixGlobalWars() {
+        System.out.println("UnixGlobalWars loaded!");
+    }
+}
