d1d765c95d9f06a2b90ad53e8118033c7974a527
IsekaiRid <mainrpg62@gmail.com>
2026-01-02 20:54:17 +0700

    setting ulang

--- src/unix/global.java
+++ src/unix/global.java
@@ -1,8 +1,8 @@
 package unix;         
 import mindustry.mod.Mod;
 
-public class World extends Mod {
-    public World(){
+public class global extends Mod {
+    public global(){
         System.out.println("UnixGlobalWars loaded!");
     }
 }
